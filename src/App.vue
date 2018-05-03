46afdd058ca218f145fdf3aa9c6c86d5c38345a6
Star <lisd.star2015@outlook.com>
2018-05-03 20:25:59 +0800

    修改 App.vue 文件(No.3)
    修改 App.vue 文件(No.3)

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
-<template lang="html">
+<template>
   <div id="app">
+    <header class="mdui-appbar mdui-appbar-fixed">
+      <div class="mdui-toolbar mdui-color-deep-purple mdui-shadow-5">
+        <span class="mdui-btn mdui-btn-icon mdui-ripple mdui-ripple-white" mdui-drawer="{target: '#main-drawer', swipe: true}">
+          <i class="mdui-icon material-icons">menu</i>
+        </span>
+        <a href="/" class="mdui-typo-headline">BOOK@HFI</a>
+        <div class="mdui-toolbar-spacer"></div>
+        <span class="mdui-btn mdui-btn-icon mdui-ripple mdui-ripple-white mdui-m-r-4" mdui-tooltip="{content: '登录/注册'}">
+          <i class="mdui-icon material-icons">account_circle</i>
+        </span>
+      </div>
+    </header>
+
     <router-view></router-view>
-    <input v-model="msg">
+
+    <footer class="mdui-bottom-nav mdui-color-deep-purple mdui-m-t-5">
+      <div class="mdui-container">
+        <div class="mdui-typo mdui-typo-caption mdui-text-center mdui-m-t-2">
+          Made with 🐱‍🚀 (这里不是抄hfinotice的！)
+        </div> 
+        <div class="mdui-typo-caption-opacity mdui-text-center">
+          Powered by HFIProgramming | 华附（国际部）编程社
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
-  data () {
-    return {
-      msg: 'Fuck U Leather Man'
-    }
-  }
 }
 </script>
+
