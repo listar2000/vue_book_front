625e0c7bea2b00256896727a87d88a1dacf0a569
Star <lisd.star2015@outlook.com>
2018-05-03 20:28:33 +0800

    添加 LeftDrawer.vue 文件(3)
    添加 LeftDrawer.vue 文件（3）

---
+++ src/components/LeftDrawer.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="main-drawer" class="mdui-drawer mdui-drawer-open">
+        <ul class="mdui-list">
+            <!-- li标签中的内容要组件化成vue文件 -->
+            <li class="mdui-list-item mdui-ripple">
+                <i class="mdui-list-item-icon mdui-icon material-icons">list</i>
+                <div class="mdui-list-item-content">我的二手书</div>
+            </li>
+            <li class="mdui-list-item mdui-ripple">
+                <i class="mdui-list-item-icon mdui-icon material-icons">list</i>
+                <div class="mdui-list-item-content">你的二手书</div>
+            </li>
+            <li class="mdui-list-item mdui-ripple">
+                <i class="mdui-list-item-icon mdui-icon material-icons">list</i>
+                <div class="mdui-list-item-content">蛤娜二手书</div>
+            </li>
+            <li class="mdui-list-item mdui-ripple">
+                <i class="mdui-list-item-icon mdui-icon material-icons">list</i>
+                <div class="mdui-list-item-content">帅分二手书</div>
+            </li>
+            <li class="mdui-list-item mdui-ripple">
+                <i class="mdui-list-item-icon mdui-icon material-icons">list</i>
+                <div class="mdui-list-item-content">卖豪二手书</div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'left-drawer',
+    }
+</script>
