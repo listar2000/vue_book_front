1a86e22c3c80ff7a0ac880fc5aa926679ea2e9ac
Star <lisd.star2015@outlook.com>
2018-05-03 20:25:13 +0800

    添加 Index.vue 文件(No.3)
    添加 Index.vue 文件(No.3)

---
+++ src/pages/Index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="index">
+        <left-drawer></left-drawer>
+        <div class="mdui-container">
+            <div class="mdui-card mdui-col-xs-12 mdui-m-t-4">
+                <div class="mdui-typo-display-3 mdui-text-center">
+                    华附国际部编程社又一巨作
+                </div>
+                <div class="mdui-typo-display-2-opacity mdui-text-center">
+                    虽然不知道有什么鬼用
+                </div>
+                <div class="mdui-typo">
+                    <hr>
+                </div>
+                <div class="mdui-typo mdui-typo-display-1 mdui-text-center">
+                    基于牛逼开源框架<a href="https://cn.vuejs.org/">Vue.js</a>构建
+                </div>
+                <img src="../assets/logo.png" class="mdui-img-rounded mdui-center">
+            </div>
+        </div>
+    </div>
+</template>
+    
+<script>
+    import LeftDrawer from '../components/LeftDrawer.vue'
+
+    export default {
+        name: 'index',
+        components: {LeftDrawer},
+    }
+</script>
