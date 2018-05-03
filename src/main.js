68693ab9127ce9530db7a0dd4fbf0b8f8d2a755b
Star <lisd.star2015@outlook.com>
2018-05-03 20:26:38 +0800

    修改 main.js 文件(No.3)
    修改 main.js 文件(No.3)

--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
-// import Vue from 'vue'
-// import VueRouter from 'vue-router'
-import App from './App.vue'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import mdui from 'mdui'
+import App from './App'
 
-// Vue.use(VueRouter)
+Vue.use(VueRouter)
 
-import homepage from './components/homepage'
-import chat from './components/chat'
+import Index from './pages/Index'
 
-const routes = [
-  {path: '/', component: homepage},
-  {path: '/chat', component: chat}
-];
+Vue.prototype.$mdui = mdui
+Vue.prototype.$bus = new Vue()
 
 const router = new VueRouter({
-  mode: 'hash',
-  routes: routes,
-});
+  mode: 'history',
+  routes: [
+    {path: '/', component: Index}
+  ]
+})
 
 new Vue({
   el: '#app',
-  router: router,
+  router,
   render: h => h(App)
 })
